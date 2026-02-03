874182353f765dd62d2e931c54d4de7a117638d6
Evelynn Lu <evie4697@gmail.com>
2026-02-02 21:31:13 -0500

    seeded fake telemetry data, front end for login /login

--- src/main/java/com/example/ecom/config/UserSeeder.java
+++ src/main/java/com/example/ecom/config/UserSeeder.java
@@ -1,5 +1,7 @@
 package com.example.ecom.config;
 
+import java.time.LocalDateTime;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,16 +9,20 @@ import com.example.ecom.repo.UserRepo;
 import com.example.ecom.entity.User;
 import com.example.ecom.entity.Device;
 import com.example.ecom.repo.DeviceRepo;
+import com.example.ecom.repo.SensorReadingRepo;
+import com.example.ecom.entity.SensorReading;
 @Component
 public class UserSeeder implements CommandLineRunner{
 
     private final UserRepo userRepo;
     private final PasswordEncoder passwordEncoder;
     private final DeviceRepo deviceRepo;
-    public UserSeeder(UserRepo userRepo,DeviceRepo deviceRepo, PasswordEncoder passwordEncoder){
+    private final SensorReadingRepo sensorReadingRepo;
+    public UserSeeder(UserRepo userRepo,DeviceRepo deviceRepo, PasswordEncoder passwordEncoder, SensorReadingRepo sensorReadingRepo){
         this.userRepo=userRepo;
         this.deviceRepo=deviceRepo;
         this.passwordEncoder=passwordEncoder;
+        this.sensorReadingRepo=sensorReadingRepo;
     }
 
     private final String initUsername = "InitialUsername";
@@ -28,19 +34,33 @@ public class UserSeeder implements CommandLineRunner{
     @Override
     public void run(String... args) {
         if(userRepo.findByUsername(initUsername).isEmpty()){
+            //SEED USER
             User user = new User();
             user.setUsername(initUsername);
             user.setPassword(passwordEncoder.encode(initPassword));
             userRepo.save(user);
             System.out.println("Database seeded with Username: " + initUsername + " Password: " + initPassword);
             
+            //SEED DEVICE
             Device device = new Device();
             device.setUser(user);
             device.setMacAddress(initMacAddress);
             device.setName(initDevice);
 
-            deviceRepo.save(device); // 3. MUST SAVE the device to the database
+            deviceRepo.save(device); 
             System.out.println("Device '" + initDevice + "' seeded for user: " + initUsername);
+
+            //SEED SENSOR READINGS
+            for(int i=0; i<10; i++){
+                SensorReading reading = new SensorReading();
+                reading.setDevice(device);
+                reading.setTemperature(20.0 + i); 
+                reading.setSoilMoisture(40.0 + i );
+                reading.setCapturedAt(LocalDateTime.now().minusHours(i));
+                sensorReadingRepo.save(reading);
+            }
+            System.out.println("Seeded 10 sensor readings for the device!");
+
         }
 
     }
